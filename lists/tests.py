a4915f041f88854c501912a5196a048ee5ed28b1
Alexander Hermann <alexander.hermann@dlr.de>
2021-08-11 09:46:26 +0200

    changed urls + include

--- lists/tests.py
+++ lists/tests.py
@@ -12,11 +12,9 @@ class NewListTest(TestCase):
 
     def test_can_save_a_POST_request(self):
         self.client.post('/lists/new', data={'item_text': 'A new list item'})
-
         self.assertEqual(Item.objects.count(), 1)
         new_item = Item.objects.first()
         self.assertEqual(new_item.text, 'A new list item')
-
 
     def test_redirects_after_POST(self):
         response = self.client.post('/lists/new', data={'item_text': 'A new list item'})
